c5b6108f23033d28acda8442ead83bcb7a400a53
Busi-Blessing <busisiwematsoele7@gmail.com>
2024-08-01 20:27:42 +0200

    All the finishes and the navigations

--- main.js
+++ main.js
@@ -38,3 +38,28 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 
 };
+
+ /**====scroll reveal=== */
+ ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+
+ });
+
+ ScrollReveal().reveal('.home-content, heading',{origin: 'top'});
+ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, contact.', { origin: 'buttom' });
+ ScrollReveal().reveal('.home-content h1, .about-img',{origin: 'left'});
+ ScrollReveal().reveal('.home-content p, .about-content',{origin: 'right'});
+
+  /**====typed js=== */
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Deveoper', 'Web designer', 'Youtuver'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+})
+
+
