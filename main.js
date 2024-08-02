45ecae9282463594c5a3d1856c4138c860cdb1e4
Busi-Blessing <busisiwematsoele7@gmail.com>
2024-08-02 08:39:29 +0200

    Changed the menu icon

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-solid fa-x');
+    menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active');
 }
 
